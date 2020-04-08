0d6fce5e8909e5d1f62f46b24be6e53a32d43792
Aziz <bemyslavedarlin@gmail.com>
2020-04-08 17:07:52 +0300

    Refactoring

--- src/backend/app/config/config.php
+++ src/backend/app/config/config.php
@@ -22,13 +22,13 @@ return new Config(
         ],
         'application' => [
             'appDir' => APP_PATH . '/',
-            'controllersDir' => APP_PATH . '/controllers/',
-            'modelsDir' => APP_PATH . '/models/',
-            'migrationsDir' => APP_PATH . '/migrations/',
-            'viewsDir' => APP_PATH . '/views/',
-            'pluginsDir' => APP_PATH . '/plugins/',
-            'libraryDir' => APP_PATH . '/library/',
-            'cacheDir' => BASE_PATH . '/cache/',
+            'controllersDir' => APP_PATH . '/Controllers/',
+            'modelsDir' => APP_PATH . '/Models/',
+            'migrationsDir' => APP_PATH . '/Migrations/',
+            'viewsDir' => APP_PATH . '/Views/',
+            'pluginsDir' => APP_PATH . '/Plugins/',
+            'libraryDir' => APP_PATH . '/Library/',
+            'cacheDir' => BASE_PATH . '/Cache/',
 
             // This allows the baseUri to be understand project paths that are not in the root directory
             // of the webpspace.  This will break if the public/index.php entry point is moved or
